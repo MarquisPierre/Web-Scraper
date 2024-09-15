c459b42f36939e51d62b553ee6f27ceb33f3a0a9
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2024-09-14 23:50:47 -0400

    attempted to connect to database

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { PriceHistoryItem } from "@/types";
+import { PriceHistoryItem, Product } from "@/types";
 
 /**
  * Extracts and returns the first found price from a list of possible elements.
@@ -101,3 +101,37 @@ export function getLowestPrice(priceList: PriceHistoryItem[]): number {
     
     return lowestPrice.price;  // Return the lowest price found
 }
+
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+    const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+    const averagePrice = sumOfPrices / priceList.length || 0;
+  
+    return averagePrice;
+  }
+  
+//   export const getEmailNotifType = (
+//     scrapedProduct: Product,
+//     currentProduct: Product
+//   ) => {
+//     const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+  
+//     if (scrapedProduct.currentPrice < lowestPrice) {
+//       return Notification.LOWEST_PRICE as keyof typeof Notification;
+//     }
+//     if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+//       return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+//     }
+//     if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+//       return Notification.THRESHOLD_MET as keyof typeof Notification;
+//     }
+  
+//     return null;
+//   };
+  
+  export const formatNumber = (num: number = 0) => {
+    return num.toLocaleString(undefined, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+  };
