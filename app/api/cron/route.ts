6227f8c09fa9b1b65096e0b7d93614aa44788f94
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2024-10-05 19:50:08 -0400

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,16 +1,19 @@
+import Product from "@/lib/models/product.model";
+import { connectToDB } from "@/lib/mongoose";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scraper";
+import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
-import { connectToDB } from "@/lib/mongoose";
-import Product from "@/lib/models/product.model";
-import { scrapeAmazonProduct } from "@/lib/scraper";
-import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+
+
+
 
 export const maxDuration = 60; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     connectToDB();
 
@@ -46,7 +49,7 @@ export async function GET(request: Request) {
           {
             url: product.url,
           },
-          product
+          product,
         );
 
         // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
