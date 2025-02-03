4ef3f7cee563390fec5fdb56c50123141368b4af
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2025-02-03 16:08:28 -0500

    added await to connectToDB() to avoid connection delay

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -12,7 +12,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
   
     try {
-      connectToDB();
+      await connectToDB();
   
       const scrapedProduct = await scrapeAmazonProduct(productUrl);
   
