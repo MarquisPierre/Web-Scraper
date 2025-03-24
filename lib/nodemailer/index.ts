1b1b0d71a1e3b8b524ec8176aa1385e988426d8a
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2025-03-23 23:44:33 -0400

    Succeeded in implementing Email engine

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -87,10 +87,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     type: "OAuth2",
     user: "marquispierre27@gmail.com",
-    // clientId: "000000000000-xxx0.apps.googleusercontent.com",
-    // clientSecret: "XxxxxXXxX0xxxxxxxx0XXxX0",
+    clientId: "Mark_Beef",
+    clientSecret: process.env.CLIENT_SECRET ,
     // refreshToken: "1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx",
-    // accessToken: "ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x",
+    accessToken: process.env.ACCESS_TOKEN,
     expires: 1484314697598,
   },
 });
