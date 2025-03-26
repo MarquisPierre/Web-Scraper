3d186d07fb86457470d9990b9a5cd718dd96272a
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2025-03-25 23:51:55 -0400

    Update index.ts

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -90,7 +90,7 @@ const transporter = nodemailer.createTransport({
     clientId: "Mark_Beef",
     clientSecret: process.env.CLIENT_SECRET ,
     accessToken: process.env.ACCESS_TOKEN,
-    expires: 1484314697598,
+    
   },
 });
 
