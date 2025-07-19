156f10c9e8ec566312a9e9af1df4a4474547cf60
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2025-07-19 17:06:14 -0400

    fixed email and email image of product problem

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -159,7 +159,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
       </div>
 
       <div className="flex flex-col gap-16">
-        {/* <div className="flex flex-col gap-5">
+         {/* <div className="flex flex-col gap-5">
           <h3 className="text-2xl text-secondary font-semibold">
             Product Description
           </h3>
@@ -167,7 +167,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
           <div className="flex flex-col gap-4">
             {product?.description?.split('\n')}
           </div>
-        </div> */}
+        </div>  */}
 
         <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]" > {/* Turn this into a link to the products orginal page*/}
           <Image 
