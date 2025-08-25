13433628cca7608fd723f995a1d37cdf77ec7f59
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2025-08-24 20:27:55 -0400

    changed send mail

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -101,10 +101,12 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) =>
     subject: emailContent.subject,
   }
 
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if(error) return console.log(error);
-    
-    console.log('Email sent: ', info);
-  })
-
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
 }
